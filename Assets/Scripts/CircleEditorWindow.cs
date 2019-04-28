a601ed5a30573984941cc27f8a9bc19b37c34ed5
Turtle_droid <mattias@kimber.se>
2019-04-28 22:14:10 +0200

    Initial commit

---
+++ Assets/Scripts/CircleEditorWindow.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(DrawCircle))]
+
+class CircleEditorWindow : EditorWindow
+{
+   
+    [SerializeField]
+    GameObject myGameObject;
+
+    [SerializeField]
+    float sides;
+
+    [SerializeField]
+    float radius;
+
+    private DrawCircle circleDrawer;
+
+    [MenuItem("Window/CircleEditor")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CircleEditorWindow));
+    }
+
+    void OnGUI()
+    {
+        // The actual window code goes here
+        myGameObject = (GameObject)EditorGUILayout.ObjectField(myGameObject, typeof(GameObject), true);
+
+        if (myGameObject)
+        {
+            circleDrawer = myGameObject.GetComponent<DrawCircle>();
+
+                if (circleDrawer)
+                {
+                    circleDrawer.segments = EditorGUILayout.IntSlider("Segment slider", circleDrawer.segments, 0, 1000);
+
+                    circleDrawer.horizRadius = EditorGUILayout.Slider("Horizontal radius slider", circleDrawer.horizRadius, 0, 10);
+
+                    circleDrawer.vertRadius = EditorGUILayout.Slider("Vertical radius slider", circleDrawer.vertRadius, 0, 10);
+                }
+        }
+    }
+}
+
---
+++ Assets/Scripts/CustomCircleEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(DrawCircle))]
+public class CustomCircleEditor : Editor
+{
+    public void OnSceneGUI()
+    {
+        DrawCircle dc = (target as DrawCircle);
+
+        EditorGUI.BeginChangeCheck();
+        //Radius handle to change... radius
+        float radius = Handles.RadiusHandle(Quaternion.identity, dc.transform.position, dc.horizRadius);
+        // Handle for changing number of segments.
+        float segments = Handles.ScaleValueHandle((float)dc.segments, dc.transform.position, Quaternion.identity, 5, Handles.ConeHandleCap, 1);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Changed Area Of Effect");
+            dc.horizRadius = radius;
+            dc.vertRadius = radius;
+            dc.segments = (int)segments;        
+        }
+    }
+}
