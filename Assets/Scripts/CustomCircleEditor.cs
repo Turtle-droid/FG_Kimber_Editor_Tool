4cc4ad2f024a423d9ccdc39bd90419be026b9035
Turtle_droid <mattias@kimber.se>
2019-04-29 12:00:28 +0200

    Tweaked custom editor

--- Assets/Scripts/CustomCircleEditor.cs
+++ Assets/Scripts/CustomCircleEditor.cs
@@ -13,12 +13,16 @@ public class CustomCircleEditor : Editor
         //Radius handle to change... radius
         float radius = Handles.RadiusHandle(Quaternion.identity, dc.transform.position, dc.horizRadius);
         // Handle for changing number of segments.
-        float segments = Handles.ScaleValueHandle((float)dc.segments, dc.transform.position, Quaternion.identity, 5, Handles.ConeHandleCap, 1);
+        float segments = Handles.ScaleValueHandle((float)dc.segments, dc.transform.position, Quaternion.identity, 5, Handles.CircleHandleCap, 1);
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(target, "Changed Area Of Effect");
             dc.horizRadius = radius;
             dc.vertRadius = radius;
+            if (segments < 0)
+            {
+                segments = 0;
+            }
             dc.segments = (int)segments;        
         }
     }
