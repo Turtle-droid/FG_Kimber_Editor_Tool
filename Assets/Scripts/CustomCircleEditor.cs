ca5aa407e46411d714adc1c748d1e9a679871015
Turtle_droid <mattias@kimber.se>
2019-04-29 16:43:57 +0200

    New editor

--- Assets/Scripts/CustomCircleEditor.cs
+++ Assets/Scripts/CustomCircleEditor.cs
@@ -5,23 +5,31 @@ using UnityEditor;
 [CustomEditor(typeof(DrawCircle))]
 public class CustomCircleEditor : Editor
 {
+    
+
     public void OnSceneGUI()
     {
         DrawCircle dc = (target as DrawCircle);
 
+        float size = HandleUtility.GetHandleSize(dc.transform.position) * 1.5f;
+        float snap = 1.0f;
+
         EditorGUI.BeginChangeCheck();
-        //Radius handle to change... radius
-        float horizRadius = Handles.RadiusHandle(Quaternion.identity, dc.transform.position, dc.horizRadius, false);
-        //float vertradius = Handles.RadiusHandle(Quaternion.identity, dc.transform.position, dc.vertRadius, true);
+
+        //Scale slider for horizontal radius
+        float horizRadius = Handles.ScaleSlider(dc.horizRadius, dc.transform.position, dc.transform.right, Quaternion.identity, size, snap);
+        //Scale slider for vertical radius
+        float vertRadius = Handles.ScaleSlider(dc.vertRadius, dc.transform.position, dc.transform.up, Quaternion.identity, size, snap);
+
         // Handle for changing number of segments.
         float segments = Handles.ScaleValueHandle((float)dc.segments, dc.transform.position, Quaternion.identity, 5, Handles.CircleHandleCap, 1);
-        if (EditorGUI.EndChangeCheck())
+       
+    if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(target, "Changed Area Of Effect");
             //Set radius for circle object
             dc.horizRadius = horizRadius;
-            dc.vertRadius = horizRadius;
-            //dc.vertRadius = vertRadius;
+            dc.vertRadius = vertRadius;
 
             // Make sure segments doesn't go below zero
             if (segments < 0)
